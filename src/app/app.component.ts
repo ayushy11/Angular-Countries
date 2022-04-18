345f2c5bed9a7d50033c0badec7b8dec6a5e04b9
AYUSH YADAV <ayushexx@gmail.com>
2022-04-18 20:24:09 +0530

    set up theme and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ThemeService, Theme } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-countries';
+export class AppComponent implements OnInit {
+  theme!: Observable<Theme>;
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.theme = this.themeService.mode$;
+  }
+
+  toggleTheme() {
+    this.themeService.toggleMode();
+  }
 }
